e98841f9f056f4c615d5f715d9b3abdbb7160ca1
dzsandzsee <dzsandzsee@gmail.com>
2024-07-03 18:56:33 +0200

    added characters from swapi

--- script.js
+++ script.js
@@ -2,7 +2,30 @@ async function fetchData() {
 
 const fetchResult = await fetch("https://swapi.dev/api/people/");
 const data = await fetchResult.json();
-console.log(data.results)
+const characters = data.results
+const rootElement = document.querySelector("#root")
+let charactersHtml = ""
+/* for (let i = 0; i < characters.length; i++) {
+    charactersHtml += `
+    <div class="character">
+    <p class="name">${characters[i].name}</p>
+    <p class="height">${characters[i].height} cm</p>
+    <p class="mass">${characters[i].mass} kg</p>
+    </div>
+    `
+    
+} */
+characters.forEach((character) => {
+    charactersHtml += `
+    <div class="character">
+    <p class="name">${character.name}</p>
+    <p class="height">${character.height} cm</p>
+    <p class="mass">${characters.mass} kg</p>
+    </div>
+    `
+    
+});
+rootElement.insertAdjacentHTML("beforeend", charactersHtml)
 }
 fetchData()
 
