be73ab0ef737304f638e2e3d500caabaf08c54aa
dzsandzsee <dzsandzsee@gmail.com>
2024-07-05 18:32:04 +0200

    cleaned up js

--- script.js
+++ script.js
@@ -1,49 +1,23 @@
-const characterComponent = (name, height, mass) => {
-    return `
+const characterComponent = (name, height, mass) => `
     <div class="character">
     <p class="name">${name}</p>
     <p class="height">${height} cm</p>
     <p class="mass">${mass} kg</p>
     </div>
     `
-}
-
 
 async function fetchData() {
 
 const fetchResult = await fetch("https://swapi.dev/api/people/");
 const data = await fetchResult.json();
 const characters = data.results
+
 const rootElement = document.querySelector("#root")
-/* for (let i = 0; i < characters.length; i++) {
-    let charactersHtml = ""
-    charactersHtml += `
-    <div class="character">
-    <p class="name">${characters[i].name}</p>
-    <p class="height">${characters[i].height} cm</p>
-    <p class="mass">${characters[i].mass} kg</p>
-    </div>
-    `
-    
-} */
-/* characters.forEach((character) => {
-    charactersHtml += `
-    <div class="character">
-    <p class="name">${character.name}</p>
-    <p class="height">${character.height} cm</p>
-    <p class="mass">${characters.mass} kg</p>
-    </div>
-    `
-    
-}); */
+
 rootElement.insertAdjacentHTML("beforeend", characters
-.map((character) =>  characterComponent(character.name, character.height, character.mass)
-    ).join(" ")
+    .map((character) =>  characterComponent(character.name, character.height, character.mass))
+    .join(" ")
 )
 }
 fetchData()
 
-
-/* fetch("https://swapi.dev/api/people/")
-    .then((response) =>  response.json())
-    .then(data => console.log(data)) */
