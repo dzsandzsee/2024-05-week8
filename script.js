edad043f705b83c514198337eab04bd2dbdb1582
dzsandzsee <dzsandzsee@gmail.com>
2024-07-05 20:09:36 +0200

    added fetch next button and added some styling

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const characterComponent = (name, height, mass) => `
     <div class="character">
+    <h2> Character: </h2>
     <p class="name">${name}</p>
     <p class="height">${height} cm</p>
     <p class="mass">${mass} kg</p>
     </div>
     `
 
+const charactersComponent = (charactersData) => `
+<div class ="characters">
+${charactersData
+    .map(characterData => characterComponent(characterData.name, characterData.height, characterData.mass))
+    .join(" ")}
+</div>`
 async function fetchData() {
 
 const fetchResult = await fetch("https://swapi.dev/api/people/");
@@ -14,10 +21,13 @@ const characters = data.results
 
 const rootElement = document.querySelector("#root")
 
-rootElement.insertAdjacentHTML("beforeend", characters
-    .map((character) =>  characterComponent(character.name, character.height, character.mass))
-    .join(" ")
-)
+rootElement.insertAdjacentHTML("beforeend", charactersComponent(characters))
+rootElement.insertAdjacentHTML("beforeend", `<button class="fetch"> Loading more... </button>`)
+
+const fetchButtonElement = document.querySelector("button.fetch")
+fetchButtonElement.addEventListener("click", () => {
+    console.log("click")
+})
 }
 fetchData()
 
