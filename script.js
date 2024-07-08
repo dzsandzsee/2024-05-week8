7402d1e97b0088404ea8773116de16db972bc3bf
dzsandzsee <dzsandzsee@gmail.com>
2024-07-08 19:58:50 +0200

    refactored code new css

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const characterComponent = (name, height, mass) => `
+let charactersData = []
+
+
+const characterComponent = (name, height, mass, index) => `
     <div class="character">
-    <h2> Character: </h2>
+    <h2> Character ${index + 1}: </h2>
     <p class="name">${name}</p>
     <p class="height">${height} cm</p>
     <p class="mass">${mass} kg</p>
@@ -10,10 +13,50 @@ const characterComponent = (name, height, mass) => `
 const charactersComponent = (charactersData) => `
 <div class ="characters">
 ${charactersData
-    .map(characterData => characterComponent(characterData.name, characterData.height, characterData.mass))
+    .map((characterData, index) => characterComponent(characterData.name, characterData.height, characterData.mass, index))
     .join(" ")}
 </div>`
-async function fetchData() {
+
+
+const fetchData = async (url) => {
+const response = await fetch(url)
+const data = await response.json()
+return data
+}
+
+const makeDomFromData = (data, rootElement) => {
+    charactersData.push(...data.results)
+
+
+    let charactersHtml = charactersComponent(charactersData)
+    const buttonHtml = `<button class="fetch"> Loading more... </button>`
+
+    rootElement.insertAdjacentHTML("beforeend", charactersHtml)
+    if (data.next) {rootElement.insertAdjacentHTML("beforeend", buttonHtml)
+
+    const buttonElement = document.querySelector("button.fetch")
+    buttonElement.addEventListener("click", async () => {
+        buttonElement.innerText = "loading next page..."
+        buttonElement.disabled = true
+        const newData =  await fetchData(data.next)
+        rootElement.innerHTML = ""
+        makeDomFromData(newData, rootElement)
+
+    })
+}
+}
+
+const init = async () => {
+    const data = await fetchData("https://swapi.dev/api/people/")
+    const rootElement = document.querySelector("#root")
+    makeDomFromData(data, rootElement)
+}
+
+init()
+
+
+
+/* async function fetchData() {
 
 const fetchResult = await fetch("https://swapi.dev/api/people/");
 const data = await fetchResult.json();
@@ -25,9 +68,23 @@ rootElement.insertAdjacentHTML("beforeend", charactersComponent(characters))
 rootElement.insertAdjacentHTML("beforeend", `<button class="fetch"> Loading more... </button>`)
 
 const fetchButtonElement = document.querySelector("button.fetch")
-fetchButtonElement.addEventListener("click", () => {
+fetchButtonElement.addEventListener("click", async () => {
     console.log("click")
-})
-}
-fetchData()
+    const newFetchResult = await fetch(data.next)
+    const newData = await newFetchResult.json()
+    console.log(newData)
+    const newCharacters = newData.results
+    fetchButtonElement.remove()
+    rootElement.insertAdjacentHTML("beforeend", charactersComponent(newCharacters))
+   
+    rootElement.insertAdjacentHTML("beforeend", `<button class="fetch"> Loading more... </button>`)
+    const newFetchButtonElement = document.querySelector("button.fetch")
+    newFetchButtonElement.addEventListener("click", () => {
+        console.log("click")
 
+    
+    }   
+)
+})
+} */
+//fetchData()
